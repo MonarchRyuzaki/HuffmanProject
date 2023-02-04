063b2b1306eba26416ceb24ee83e7f9d3b59b7d6
MonarchRyuzaki <shivamganguly2004@gmail.com>
2023-02-04 10:59:21 +0530

    Encoding is Working

--- src/HuffmanAlgorithm.java
+++ src/HuffmanAlgorithm.java
@@ -19,77 +19,20 @@ public class HuffmanAlgorithm{
             this.left = null;
             this.right = null;
         }
-
+        
         @Override
         public int compareTo(HuffmanNode o) {
             
             return this.f-o.f;
         }
     }
-
-    static PriorityQueue<HuffmanNode> pq  = new PriorityQueue<>();
-    static HashMap<Character,String> hm = new HashMap<>();
-    static HuffmanNode root = null;
-
-    public static void constructHuffmanTree(){
-
-        while (pq.size()>1){
-            HuffmanNode x = pq.poll();
-            HuffmanNode y = pq.poll();
-            HuffmanNode newNode = new HuffmanNode('-', x.f+y.f);
-            newNode.left = x;
-            newNode.right = y;
-            root = newNode;
-            pq.add(newNode);
-        }
-
-    }
     
-    public static void createPrefixCodes(HuffmanNode root,String s){
-        if (s.length() == 0){
-            return;
-        }
-
-        if (root.left==null && root.right == null){
-            hm.put(root.ch,s);
-            return;
-        }
-
-        createPrefixCodes(root.left,s+"0");
-        s=s.substring(0,s.length()-1);
-        createPrefixCodes(root.right,s+"1");
-        s=s.substring(0,s.length()-1);
-    }
-
-    // static void buildCodeMap(HuffmanNode node, StringBuilder code) {
-    //     if (node == null)
-    //         return;
-
-    //     if (node.ch != '\0') {
-    //         hm.put(node.ch, code.toString());
-    //         return;
-    //     }
-
-    //     buildCodeMap(node.left, code.append("0"));
-    //     code.deleteCharAt(code.length() - 1);
-    //     buildCodeMap(node.right, code.append("1"));
-    //     code.deleteCharAt(code.length() - 1);
-    // }
-
-    public static String printEncodedText(String str){
-
-        StringBuilder v = new StringBuilder();
-        for (Character c : str.toCharArray()){
-            v.append(hm.get(c));
-        }
-
-        return v.toString();
-    }
-
-    public static void main(String[] args) {
-        
-
-        StringBuilder str = new StringBuilder();
+    static HuffmanNode root = null;
+    static PriorityQueue<HuffmanNode> pq  = new PriorityQueue<>();      //Priority Queue to Store the Huffman Nodes. It is used to construct the Huffman Tree
+    static HashMap<Character,String> hm = new HashMap<>();              //HashMap to Store the character with their Prefix Codes
+    static StringBuilder str = new StringBuilder();                     //Stores the String that is read from the File
+    
+    public static void readFromFile(String fileName){                   //Reads from the file and Converts all the data to a String
         File myFile = new File("test.txt");
         try {
             Scanner sc = new Scanner(myFile);
@@ -99,44 +42,92 @@ public class HuffmanAlgorithm{
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        HashMap<Character,Integer> map = new HashMap<>();
+    }
+    
+    public static void initializingHashMap(){
+        HashMap<Character,Integer> map = new HashMap<>();           //Intializing The HashMap with char and their Frequencies (map stores char with their frequencies)
         for (int i=0;i<str.length();i++){
             char ch = str.charAt(i);
             map.put(ch,map.getOrDefault(ch, 0)+1);
         }
-
-        for (char c : map.keySet()){
+        for (char c : map.keySet()){                                //Creating Huffman Nodes from the map HM and adding it to the PriorityQueue pq
             pq.add(new HuffmanNode(c, map.get(c)));
         }
+    }
 
-        constructHuffmanTree();
-        createPrefixCodes(root, "");
-        //System.out.println(hm);
-        //buildCodeMap(root, new StringBuilder());
+    public static void constructHuffmanTree(){                      //Constructs The Huffman Tree
+        
+        while (pq.size()>1){
+            HuffmanNode x = pq.poll();
+            HuffmanNode y = pq.poll();
+            HuffmanNode newNode = new HuffmanNode('\0', x.f+y.f);
+            newNode.left = x;
+            newNode.right = y;
+            root = newNode;
+            pq.add(newNode);
+        }
+
+    }
+
+    public static void createPrefixCodes(HuffmanNode node, StringBuilder code) {    //Creates the Prefix Codes for each Character and Stores it in HashMap hm
+
+        if (node == null){
+            return;
+        }
+
+        if (node.ch != '\0') {
+            hm.put(node.ch, code.toString());
+            return;
+        }
+
+        createPrefixCodes(node.left, code.append("0"));
+        code.deleteCharAt(code.length() - 1);
+        createPrefixCodes(node.right, code.append("1"));
+        code.deleteCharAt(code.length() - 1);
+    }
+
+    public static String printEncodedText(String str){          // Accepts The String read from the File
+
+        StringBuilder v = new StringBuilder();                  // Appends the corresponding String (from HashMap) for the character 
+        for (Character c : str.toCharArray()){
+            v.append(hm.get(c));
+        }
+
+        return v.toString();                                   // Returns the encoded String
+    }
+ 
+    public static void writeToFile(String fileName){           // Writes the Encoded String on the File
+
         String v = printEncodedText(str.toString());
-        File myFile1 = new File("Compressed test.txt");
         try {
-            //myFile1.createNewFile();
-            FileOutputStream fileWriter = new FileOutputStream("Compressed test.txt");
+            FileOutputStream fileWriter = new FileOutputStream(fileName);
             int currentByte = 0;
             int bitCounter = 0;
             for (char bit : v.toCharArray()) {
-            currentByte = (currentByte << 1) | (bit - '0');
-            bitCounter++;
-            if (bitCounter == 8) {
-                fileWriter.write(currentByte);
-                currentByte = 0;
-                bitCounter = 0;
+                currentByte = (currentByte << 1) | (bit - '0'); //Converts 0's and 1's to be written on the File
+                bitCounter++;
+                if (bitCounter == 8) {
+                    fileWriter.write(currentByte);
+                    currentByte = 0;
+                    bitCounter = 0;
+                }
             }
             if (bitCounter > 0) {
                 currentByte <<= 8 - bitCounter;
                 fileWriter.write(currentByte);
             }
-        }
-        fileWriter.close();
+            fileWriter.close();
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        
+    }
+    public static void main(String[] args) {
+        
+        readFromFile("test.txt");
+        initializingHashMap();
+        constructHuffmanTree();
+        createPrefixCodes(root, new StringBuilder());
+        writeToFile("Compressed test.txt");
     }
 }
